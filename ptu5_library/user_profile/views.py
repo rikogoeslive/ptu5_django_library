f15883f945cc9ddaeb8df62572528946daa64427
Riko <mntsklp@gmail.com>
2022-11-17 11:46:59 +0200

    user registration view; user registration form, processing and template; django messages

--- ptu5_library/user_profile/views.py
+++ ptu5_library/user_profile/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+from django.core.validators import validate_email
 
-# Create your views here.
+User = get_user_model()
+
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        error = False
+        if not username or User.objects.filter(username=username).first():
+            messages.error(request, 'Username not entered or username already exists.')
+            error = True
+        if not email or User.objects.filter(email=email).first():
+            messages.error(request, 'Email not entered or user with this email already exists')
+            error = True
+        else:
+            try:
+                validate_email(email)
+            except:
+                messages.error(request, 'Invalid email.')
+                error = True
+        if not password or not password2 or password != password2:
+            messages.error(request, 'Passwords not entered or do not match.')
+        if not error:
+            User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, f'User {username} registration succesful, You can log in now.')
+            return redirect('login')
+    return render(request, 'user_profile/register.html')
