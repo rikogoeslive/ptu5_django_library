e83a319cc251f78f0f7f7afa5e0f5b7ad79a1f60
Riko <mntsklp@gmail.com>
2022-11-17 15:51:21 +0200

    review form with anti flood validation

--- ptu5_library/library/views.py
+++ ptu5_library/library/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormMixin
+from django.urls import reverse
 from . models import Genre, Author, Book, BookInstance
+from . forms import BookReviewForm
 
 # Create your views here.
 
@@ -70,6 +72,38 @@ class BookListView(ListView):
 class BookDetailView(FormMixin, DetailView):
     model = Book
     template_name = 'library/book_detail.html'
+    form_class = BookReviewForm
+
+    def get_success_url(self):
+        return reverse('book', kwargs={'pk': self.get_object().id})
+
+    def post(self, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            messages.error(self.request, "You're posting too much!")
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.book = self.get_object()
+        form.instance.reader = self.request.user
+        form.save()
+        messages.success(self.request, 'Your Review have been posted ')
+        return super().form_valid(form)
+
+    def get_initial(self):
+        return {
+            'book': self.get_object(),
+            'reader': self.request.user,
+        }
+    
+    
+    
+    # def form_valid(self, form):
+    #     form.instance.book = self.object
+
 
 
 class UserBookListView(LoginRequiredMixin, ListView):
